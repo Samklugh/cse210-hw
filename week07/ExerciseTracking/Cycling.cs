041aab9c13ca1b33507b2b0bfc1f917bab65e4ce
Samson Oladimeji Odunaiya <102425294+Samklugh@users.noreply.github.com>
2026-04-14 22:35:43 +0100

    assignment

---
+++ week07/ExerciseTracking/Cycling.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Cycling : Activity
+{
+    private double _speed; // mph
+
+    public Cycling(DateTime date, int minutes, double speed) : base(date, minutes)
+    {
+        _speed = speed;
+    }
+
+    public override double GetDistance() => (_speed / 60) * Minutes;
+    public override double GetSpeed() => _speed;
+    public override double GetPace() => 60 / _speed;
+}
